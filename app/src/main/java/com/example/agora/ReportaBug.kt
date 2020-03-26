ab045f52ec3d430f156c03954cf65e42a7f62a75
prince-09 <princesinha7072@gmail.com>
2020-03-26 16:45:03 +0530

    more pages added

---
+++ app/src/main/java/com/example/agora/ReportaBug.kt
@@ -0,0 +1,12 @@
+package com.example.agora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportaBug : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reporta_bug)
+    }
+}
